93357cef629126b034d214ed43a52006f53684db
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-10 19:50:49 +0200

    Cleanup classpath.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,11 @@ subprojects {
             resolutionStrategy {
                 force("com.fasterxml.jackson.core:jackson-core:2.13.3")
                 force("org.junit.jupiter:junit-jupiter-api:5.8.2")
+                force("org.apache.logging.log4j:log4j-core:2.17.2")
+                force("org.apache.logging.log4j:log4j-jul:2.17.2")
+                force("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
+                force("ch.qos.logback:logback-classic:1.2.11")
+                force("org.slf4j:slf4j-api:1.7.36")
                 force("org.ow2.asm:asm:9.2")
                 force("org.ow2.asm:asm-analisys:9.2")
                 force("org.ow2.asm:asm-commons:9.2")
