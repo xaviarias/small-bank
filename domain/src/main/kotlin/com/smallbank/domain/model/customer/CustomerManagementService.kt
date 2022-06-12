170bbb59d480ce5f54dfff6c0d3eb28dfbb168ac
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-13 00:38:58 +0200

    Add customer management unit tests

--- domain/src/main/kotlin/com/smallbank/domain/model/customer/CustomerManagementService.kt
+++ domain/src/main/kotlin/com/smallbank/domain/model/customer/CustomerManagementService.kt
@@ -6,5 +6,7 @@ package com.smallbank.domain.model.customer
 interface CustomerManagementService {
     fun create(customer: Customer): Customer
     fun findById(customerId: CustomerId): Customer
+
+    // TODO Support pagination, this could grow big
     fun findAll(): List<Customer>
 }
---
+++ domain/src/test/kotlin/com/smallbank/domain/model/customer/CustomerManagementServiceTest.kt
@@ -0,0 +1,66 @@
+package com.smallbank.domain.model.customer
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.stub
+import org.mockito.kotlin.verify
+import java.util.UUID
+
+/**
+ * TODO Exhaustive tests.
+ */
+class CustomerManagementServiceTest {
+
+    private val customer = Customer(
+        CustomerId(UUID.randomUUID().toString()),
+        "john.doe@smail.com",
+        PersonalName("John", "Doe"),
+        PersonalAddress(
+            "Baker Street",
+            "221B",
+            "NW1",
+            "London",
+            "GB"
+        )
+    )
+
+    private lateinit var service: CustomerManagementService
+    private lateinit var repository: CustomerRepository
+
+    @BeforeEach
+    fun setUp() {
+        repository = mock {}
+        service = CustomerManagementServiceImpl(repository)
+    }
+
+    @Test
+    fun `create should save a new customer in the repository`() {
+        service.create(customer)
+        verify(repository).save(customer)
+    }
+
+    @Test
+    fun `find by id should return the customer from the repository`() {
+        repository.stub {
+            on { findById(customer.id) } doReturn customer
+        }
+
+        val returnCustomer = service.findById(customer.id)
+        Assertions.assertEquals(customer, returnCustomer)
+        verify(repository).findById(customer.id)
+    }
+
+    @Test
+    fun `find all should return all customers from the repository`() {
+        repository.stub {
+            on { findAll() } doReturn listOf(customer)
+        }
+
+        val allCustomers = service.findAll()
+        Assertions.assertEquals(listOf(customer), allCustomers)
+        verify(repository).findAll()
+    }
+}
