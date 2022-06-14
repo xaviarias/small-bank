f98f97769cc0965fdb6db96c6715c0c08474ef1a
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-14 04:13:48 +0200

    Make vault classes public to be used from app

--- infra/src/main/kotlin/com/smallbank/infra/ethereum/EthereumKeyVault.kt
+++ infra/src/main/kotlin/com/smallbank/infra/ethereum/EthereumKeyVault.kt
@@ -5,7 +5,7 @@ import org.web3j.crypto.Credentials
 /**
  * Customer key repository to store sensitive data externally.
  */
-internal interface EthereumKeyVault {
+interface EthereumKeyVault {
     fun store(credentials: Credentials)
     fun resolve(account: String): Credentials?
 }
---
+++ infra/src/main/kotlin/com/smallbank/infra/ethereum/InMemoryEthereumKeyVault.kt
@@ -0,0 +1,23 @@
+package com.smallbank.infra.ethereum
+
+import org.springframework.stereotype.Repository
+import org.web3j.crypto.Credentials
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+
+/**
+ * Default in-memory Ethereum key vault.
+ *
+ * In a real scenario this would typically be an external system, e.g. KMS.
+ */
+@Repository
+internal open class InMemoryEthereumKeyVault : EthereumKeyVault {
+
+    private val keys: ConcurrentMap<String, Credentials> = ConcurrentHashMap()
+
+    override fun store(credentials: Credentials) {
+        keys.putIfAbsent(credentials.ecKeyPair.publicKey.toHexString(), credentials)
+    }
+
+    override fun resolve(account: String) = keys[account]
+}
