d3d070a680724d810eb6bf1488df426cf2f07cbb
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-15 02:04:36 +0200

    Fix Gradle build

--- infra/src/main/kotlin/com/smallbank/infra/SmallBankApplication.kt
+++ infra/src/main/kotlin/com/smallbank/infra/SmallBankApplication.kt
@@ -1,8 +1,13 @@
 package com.smallbank.infra
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @SpringBootApplication
 open class SmallBankApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmallBankApplication>(*args)
+}
