ccee1bbe3388afdd9f09a597da1448a0325559aa
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-15 03:38:05 +0200

    Cleanup unused annotations

--- infra/src/main/kotlin/com/smallbank/infra/SmallBankApplication.kt
+++ infra/src/main/kotlin/com/smallbank/infra/SmallBankApplication.kt
@@ -2,9 +2,7 @@ package com.smallbank.infra
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @SpringBootApplication
 open class SmallBankApplication
 
