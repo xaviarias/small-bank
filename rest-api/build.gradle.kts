b00ff5a1b2c27ff8857621e79683f3539e363f0e
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-20 00:23:29 +0200

    Add SPringDoc to generate Open API 3 page

--- rest-api/build.gradle.kts
+++ rest-api/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.web3j:core:4.9.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.web3j:web3j-unit:4.9.2")
