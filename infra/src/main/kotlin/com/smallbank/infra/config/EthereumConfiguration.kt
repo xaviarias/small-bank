fa58536ffa53cd5f57daac5626b9db23d8ab0766
Xavier Arias <xavi.arias.segui@gmail.com>
2022-06-19 18:07:53 +0200

    Support Görli Testnet as default configuration

--- infra/src/main/kotlin/com/smallbank/infra/config/EthereumConfiguration.kt
+++ infra/src/main/kotlin/com/smallbank/infra/config/EthereumConfiguration.kt
@@ -3,35 +3,48 @@ package com.smallbank.infra.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Scope
-import org.springframework.stereotype.Component
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.Web3jService
 import org.web3j.protocol.http.HttpService
 import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.tx.gas.DefaultGasProvider
 
-@Component
-internal class EthereumConfiguration {
+@Configuration
+internal open class EthereumConfiguration {
 
     @Value("\${smallbank.ethereum.url:${HttpService.DEFAULT_URL}}")
     private var url: String? = null
 
+    @Value("\${smallbank.ethereum.auth-token:#{null}}")
+    private var authToken: String? = null
+
     @Bean
     @Scope(BeanDefinition.SCOPE_SINGLETON)
-    fun web3j(service: Web3jService): Web3j {
+    open fun web3j(service: Web3jService): Web3j {
         return Web3j.build(service)
     }
 
     @Bean
     @Scope(BeanDefinition.SCOPE_SINGLETON)
-    fun web3jService(): Web3jService {
-        return HttpService(url)
+    open fun web3jService(): Web3jService {
+        val clientBuilder = HttpService.getOkHttpClientBuilder()
+        if (authToken != null) {
+            clientBuilder.addInterceptor {
+                it.proceed(
+                    it.request().newBuilder() // Set authorization header
+                        .header("Authorization", "Bearer $authToken")
+                        .build()
+                )
+            }
+        }
+        return HttpService(url, clientBuilder.build())
     }
 
     @Bean
     @Scope(BeanDefinition.SCOPE_SINGLETON)
-    fun gasProvider(): ContractGasProvider {
+    open fun gasProvider(): ContractGasProvider {
         return DefaultGasProvider()
     }
 }
